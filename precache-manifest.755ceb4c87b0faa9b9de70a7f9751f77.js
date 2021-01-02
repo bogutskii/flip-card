14164bdcf8b2628e3db0bfbfcf11883224629a18
bogutskii <sl9site@gmail.com>
2021-01-02 10:17:00 -0800

    Updates

---
+++ precache-manifest.755ceb4c87b0faa9b9de70a7f9751f77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6a61502264c675cbf163bf349ebb17b",
+    "url": "/flip-card/index.html"
+  },
+  {
+    "revision": "6e003b38d5c8f567c2cc",
+    "url": "/flip-card/static/css/main.7383b553.chunk.css"
+  },
+  {
+    "revision": "a16d19c9602e0e29ba94",
+    "url": "/flip-card/static/js/2.bcaf2abd.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/flip-card/static/js/2.bcaf2abd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e003b38d5c8f567c2cc",
+    "url": "/flip-card/static/js/main.0a7d3ab1.chunk.js"
+  },
+  {
+    "revision": "63de4ef57cd6993c2b40",
+    "url": "/flip-card/static/js/runtime-main.394a751f.js"
+  },
+  {
+    "revision": "b6dbfefd82a56c23741a70dc7a146ffc",
+    "url": "/flip-card/static/media/reverse.b6dbfefd.png"
+  }
+]);
